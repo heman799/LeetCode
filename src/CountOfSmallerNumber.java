3ad22ff9ab1ebb4922127fc52ac2e34719d2b6bd
sldevmac1 <sldevmac1@sldevmac1s-MacBook-Pro.local>
2019-06-22 15:44:13 -0500

    add CountOfSmallerNumber.java

--- src/CountOfSmallerNumber.java
+++ src/CountOfSmallerNumber.java
@@ -1,5 +1,25 @@
 public class CountOfSmallerNumber {
 
+    public List<Integer> countOfSmallerNumber(int[] A, int[] queries) {
+        if (queries == null || queries.length == 0) {
+            return new ArrayList<Integer>();
+        }
+
+        List<Integer> ans = new ArrayList<>(queries.length);
+
+        int lenA = (A == null) ? 0 : A.length;
+
+        Arrays.sort(A);
+        for (int query : queries) {
+            if (lenA == 0) {
+                ans.add(0);
+            } else {
+                ans.add(binarySearch(A, query));
+            }
+        }
+
+        return ans;
+    }
     private int binarySearch(int[]A, int target) {
         int start = 0;
         int end = A.length - 1;
